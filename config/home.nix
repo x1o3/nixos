0518eab56dc9107db7e57f80383363aa4a947088
x1o3 <jat1n.9013k@gmail.com>
2025-08-18 22:29:52 +0530

    added zen

--- config/home.nix
+++ config/home.nix
@@ -3,6 +3,10 @@
 {
   home.username = "x1o3";
   home.homeDirectory = "/home/x1o3";
+  home.packages = with pkgs; [
+    inputs.zen-browser.packages."${system}".specific
+  ];
   home.stateVersion = "25.05"; 
+  programs.home-manager.enable = true;
 }
 
