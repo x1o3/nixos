b2a861175877117c5f1145458a55714426c73942
x1o3 <jat1n.9013k@gmail.com>
2025-08-18 22:52:19 +0530

    added zen

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "very smol snowflake";
 
   inputs = {
-    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -10,29 +10,22 @@
     };
   };
 
-  outputs = { nixpkgs, zen-browser, home-manager, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    username = "x1o3";
-    dir = "/home/$(username)";
-    stateVersion = "25.05";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-      nixosConfigurations.nyx = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./config/configuration.nix
-	      home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.backupFileExtension = "HMBackup";
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs system pkgs username dir stateVersion; };
-            home-manager.users.x1o3.imports = [
-              ./config/home.nix
-            ];
-          }
-        ];
-      };
+  outputs = { nixpkgs, home-manager, zen-browser, ... } @ inputs:{
+    nixosConfigurations.nyx = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./config/configuration.nix
+	    home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.backupFileExtension = "HMBackup";
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
+          home-manager.users.quiet.imports = [
+            ./config/home.nix
+          ];
+        }
+      ];
     };
+  };
 }
