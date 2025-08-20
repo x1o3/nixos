ad5c0cdd8f13754c4db673970e677d7741af2d9e
x1o3 <jat1n.9013k@gmail.com>
2025-08-20 21:11:10 +0530

    fixing mountpts

--- config/modules/mount.nix
+++ config/modules/mount.nix
@@ -4,11 +4,5 @@
    device = "/dev/disk/by-uuid/72D1B72F7624D579";
    fsType = "ntfs3";
    options = [ "defaults" ];
-  };
-  
-  fileSystems."/xbackup" = {
-   device = "/dev/disk/by-uuid/AAA4681CA467E971";
-   fsType = "ntfs3";
-   options = [ "defaults" ];
-  };
+  };  
 }
