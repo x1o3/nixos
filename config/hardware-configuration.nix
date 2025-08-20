235fb94151fcf25b98e18802384c493995eeffd5
x1o3 <jat1n.9013k@gmail.com>
2025-08-20 21:06:13 +0530

    cpu gov -> performance

--- config/hardware-configuration.nix
+++ config/hardware-configuration.nix
@@ -8,6 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  powerManagement.cpuFreqGovernor = "performance";  
   boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
