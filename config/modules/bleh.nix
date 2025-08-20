1f1719d9864248c1457b30506c359c7cd7f73181
Jatin Kakkar <148622748+x1o3@users.noreply.github.com>
2025-08-20 19:18:59 +0530

    removed mounts

--- config/modules/bleh.nix
+++ config/modules/bleh.nix
@@ -1,17 +1,5 @@
 { lib, pkgs, ...}: {
 
-  fileSystems."/x" = {
-   device = "/dev/disk/by-uuid/7902EACA6C144EBC";
-   fsType = "ntfs3";
-   options = [ "defaults" ];
-  };
-  
-  fileSystems."/xbackup" = {
-   device = "/dev/disk/by-uuid/AAA4681CA467E971";
-   fsType = "ntfs3";
-   options = [ "defaults" ];
-  };
-
   boot = {
     kernelPackages = pkgs.linuxPackages_6_12;
     kernelParams = [
